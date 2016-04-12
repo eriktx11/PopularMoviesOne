4dcf29929ae915e0cb2991af35579814ace7ab37
eriktx11 <erik@llerenapiano.com>
2016-04-12 15:33:16 -0500

    crash2

--- app/src/main/java/com/example/android/popularmoviesone/MainActivityFragment.java
+++ app/src/main/java/com/example/android/popularmoviesone/MainActivityFragment.java
@@ -4,12 +4,16 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.text.format.Time;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -40,7 +44,9 @@ public class MainActivityFragment extends Fragment {
     }
 
     private ArrayAdapter<String> mMovietAdapter;
+
     @Override
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
@@ -51,8 +57,10 @@ public class MainActivityFragment extends Fragment {
                 new ArrayList<String>()
         );
 
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        GridView gridView = (GridView) rootView.findViewById(R.id.gridView);
+        gridView.setAdapter(mMovietAdapter);
         return rootView;
 
 
@@ -62,7 +70,6 @@ public class MainActivityFragment extends Fragment {
     public class FetchMovieList extends AsyncTask<String, Void, String[]> {
 
         private final String LOG_TAG = FetchMovieList.class.getSimpleName();
-
 
 
         private String[] getMovieDataFromJson(String moviesJsonStr)
@@ -98,113 +105,120 @@ public class MainActivityFragment extends Fragment {
         }
 
 
-    @Override
-    protected String[] doInBackground(String... params) {
+        @Override
+        protected String[] doInBackground(String... params) {
 
-        // If there's no zip code, there's nothing to look up.  Verify size of params.
-        if (params.length == 0) {
-            return null;
-        }
-
-        // These two need to be declared outside the try/catch
-        // so that they can be closed in the finally block.
-        HttpURLConnection urlConnection = null;
-        BufferedReader reader = null;
-
-        // Will contain the raw JSON response as a string.
-        String moviesJsonStr = null;
-
-        String format = "json";
-        //String units = "metric";
-        //int numDays = 7;
-
-        try {
-            // Construct the URL for the OpenWeatherMap query
-            // Possible parameters are avaiable at OWM's forecast API page, at
-            // http://openweathermap.org/API#forecast
-            final String FORECAST_BASE_URL =
-                    "http://api.themoviedb.org/3/movie/popular?api_key=";
-            //final String QUERY_PARAM = "q";
-            //final String FORMAT_PARAM = "mode";
-            //final String UNITS_PARAM = "units";
-            //final String DAYS_PARAM = "cnt";
-            final String APPID_PARAM = "APPID";
-
-            Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
-                    //.appendQueryParameter(QUERY_PARAM, params[0])
-                    //.appendQueryParameter(FORMAT_PARAM, format)
-                    //.appendQueryParameter(UNITS_PARAM, units)
-                    //.appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
-                    .appendQueryParameter(APPID_PARAM, BuildConfig.OPEN_MOVIE_API_KEY)
-                    .build();
-
-            URL url = new URL(builtUri.toString());
-
-            // Create the request to OpenWeatherMap, and open the connection
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
-            urlConnection.connect();
-
-            // Read the input stream into a String
-            InputStream inputStream = urlConnection.getInputStream();
-            StringBuffer buffer = new StringBuffer();
-            if (inputStream == null) {
-                // Nothing to do.
+            // If there's no zip code, there's nothing to look up.  Verify size of params.
+            if (params.length == 0) {
                 return null;
             }
-            reader = new BufferedReader(new InputStreamReader(inputStream));
 
-            String line;
-            while ((line = reader.readLine()) != null) {
-                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                // But it does make debugging a *lot* easier if you print out the completed
-                // buffer for debugging.
-                buffer.append(line + "\n");
-            }
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
 
-            if (buffer.length() == 0) {
-                // Stream was empty.  No point in parsing.
+            // Will contain the raw JSON response as a string.
+            String moviesJsonStr = null;
+
+            String format = "json";
+            //String units = "metric";
+            //int numDays = 7;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                final String FORECAST_BASE_URL =
+                        "http://api.themoviedb.org/3/movie/popular?";
+                //final String QUERY_PARAM = "q";
+                //final String FORMAT_PARAM = "mode";
+                //final String UNITS_PARAM = "units";
+                //final String DAYS_PARAM = "cnt";
+                final String APPID_PARAM = "api_key";
+
+                Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                        //.appendQueryParameter(QUERY_PARAM, params[0])
+                        //.appendQueryParameter(FORMAT_PARAM, format)
+                        //.appendQueryParameter(UNITS_PARAM, units)
+                        //.appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                        .appendQueryParameter(APPID_PARAM, BuildConfig.OPEN_MOVIE_API_KEY)
+                        .build();
+
+                URL url = new URL(builtUri.toString());
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                moviesJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
                 return null;
-            }
-            moviesJsonStr = buffer.toString();
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "Error ", e);
-            // If the code didn't successfully get the weather data, there's no point in attemping
-            // to parse it.
-            return null;
-        } finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (final IOException e) {
-                    Log.e(LOG_TAG, "Error closing stream", e);
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
                 }
             }
-        }
 
-        try {
-            return getMovieDataFromJson(moviesJsonStr);
-        } catch (JSONException e) {
-            Log.e(LOG_TAG, e.getMessage(), e);
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    @Override
-    protected void onPostExecute(String[] result) {
-        if (result != null) {
-            mMovietAdapter.clear();
-            for(String dayForecastStr : result) {
-                mMovietAdapter.add(dayForecastStr);
+            try {
+                return getMovieDataFromJson(moviesJsonStr);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+                e.printStackTrace();
             }
-            //new data coming in
+            return null;
         }
-    }
+
+
+        private ImageView imageView;
+        @Override
+        protected void onPostExecute(String[] result) {
+
+
+            imageView = (ImageView) imageView.findViewById(R.id.imageViewMovie);
+            if (result != null) {
+                mMovietAdapter.clear();
+                for (String thumbMovie : result) {
+                    mMovietAdapter.add(thumbMovie);
+                    Picasso.with(getContext()).load(thumbMovie).into(imageView);
+                    //new data coming in
+                }
+            }
+
+        }
 
     }
-
 }
+
