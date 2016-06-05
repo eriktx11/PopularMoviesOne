ecd346303885cead963d8b6e55c97f721a19cbcd
eriktx11 <erik@llerenapiano.com>
2016-06-05 01:14:33 -0500

    nested scrolling not working

--- app/src/main/java/com/example/android/popularmoviesone/MainGrid.java
+++ app/src/main/java/com/example/android/popularmoviesone/MainGrid.java
@@ -3,6 +3,7 @@ package com.example.android.popularmoviesone;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
@@ -34,12 +35,23 @@ public class MainGrid extends ActionBarActivity implements ForecastFragment.Call
 
     private boolean mTwoPane;
 
+
+    public static boolean isTablet(Context context) {
+        return (context.getResources().getConfiguration().screenLayout
+                & Configuration.SCREENLAYOUT_SIZE_MASK)
+                >= Configuration.SCREENLAYOUT_SIZE_LARGE;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+        boolean res = isTablet(getBaseContext());
         setContentView(R.layout.main_grid);
-        if (findViewById(R.id.movie_detail_container) != null) {
+        //getResources().getBoolean(R.bool.isTab)
+        //findViewById(R.id.movie_detail_container) != null &&
+        if (isTablet(getBaseContext()))  {
             // The detail container view will be present only in the large-screen layouts
             // (res/layout-sw600dp). If this view is present, then the activity should be
             // in two-pane mode.
@@ -65,50 +77,6 @@ public class MainGrid extends ActionBarActivity implements ForecastFragment.Call
         MovieSyncAdapter.initializeSyncAdapter(this);
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//
-////
-//          if (id == R.id.sortP) {
-////            sortSelect=1;
-////            new FetchMovieList().execute("http://api.themoviedb.org/3/movie/popular");
-//              startActivity(new Intent(this, SettingsActivity.class));
-//              return true;
-//        }
-//        if (id == R.id.sortR) {
-////            sortSelect=2;
-////            new FetchMovieList().execute("http://api.themoviedb.org/3/movie/top_rated");
-//            startActivity(new Intent(this, SettingsActivity.class));
-//            return true;
-//        }
-//
-//
-//
-
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            startActivity(new Intent(this, SettingsActivity.class));
-//            return true;
-//        }
-//
-//        if (id == R.id.action_map) {
-//            openPreferredLocationInMap();
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
 
     @Override
     protected void onResume() {
